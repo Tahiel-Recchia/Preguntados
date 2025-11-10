a641ab0646d88ce8d3f4c4773c37fac5caa00946
ELIAS2401 <tuccieliasluis@gmail.com>
2025-11-10 20:46:46 -0300

    editarUbicacion en perfil y qr perfil
    falta que se pueda ver desde otro usuario el perfil

--- model/PerfilModel.php
+++ model/PerfilModel.php
@@ -1,21 +1,34 @@
 <?php
 
-class PerfilModel {
+class PerfilModel
+{
     private $conexion;
 
-    public function __construct($conexion){
+    public function __construct($conexion)
+    {
         $this->conexion = $conexion;
     }
 
-    public function getDatosUsuario($idUsuario){
-    $sql = "SELECT id, nombreDeUsuario, fechaNac, fotoDePerfil, sexo, direccion, puntajeAcumulado, rol_id FROM usuario WHERE id = ?";
-    $stmt = $this->conexion->prepare($sql);
-    $stmt->bind_param("i", $idUsuario);
-    $stmt->execute();
-    $resultado = $stmt->get_result();
-    $fila = $resultado->fetch_assoc();
-    $stmt->close();
+    public function getDatosUsuario($idUsuario)
+    {
+        $sql = "SELECT id, nombreDeUsuario, fechaNac, fotoDePerfil, sexo, direccion, puntajeAcumulado, rol_id FROM usuario WHERE id = ?";
+        $stmt = $this->conexion->prepare($sql);
+        $stmt->bind_param("i", $idUsuario);
+        $stmt->execute();
+        $resultado = $stmt->get_result();
+        $fila = $resultado->fetch_assoc();
+        $stmt->close();
 
-    return $fila;
+        return $fila;
     }
+
+    public function actualizarDireccion($userId, $direccion)
+    {
+        $sql = "UPDATE usuario SET direccion = ? WHERE id = ?";
+        $stmt = $this->conexion->prepare($sql);
+        $stmt->bind_param("si", $direccion, $userId);
+        $stmt->execute();
+        $stmt->close();
+    }
+
 }
